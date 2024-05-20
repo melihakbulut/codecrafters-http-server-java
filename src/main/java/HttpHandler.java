4fa031025078b147fa25f840865b00648626f1d6
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-20 14:33:17 +0300

    test

--- src/main/java/HttpHandler.java
+++ src/main/java/HttpHandler.java
@@ -3,11 +3,12 @@ import java.net.Socket;
 
 public class HttpHandler {
 
-    public static final String NEW_LINE = "\r\n\r\n";
+    //    public static final String NEW_LINE = "\r\n\r\n";
+    public static final String NEW_LINE = "\r\n";
     public static final String SUCCESS = "HTTP/1.1 200 OK" + NEW_LINE;
     public static final String NOT_FOUND = "HTTP/1.1 404 Not Found" + NEW_LINE;
 
-    public void handle(Socket clientSocket) throws IOException {
+    public void handle(Socket clientSocket) throws IOException, InterruptedException {
         HttpRequest httpRequest = HttpParser.parse(clientSocket);
         String answer = null;
         if (httpRequest.getEndpoint().isEmpty())
@@ -21,13 +22,14 @@ public class HttpHandler {
             answer = NOT_FOUND;
         System.out.println(answer);
         clientSocket.getOutputStream().write(answer.getBytes());
+        Thread.sleep(10);
     }
 
     public String sendEcho(String value) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(SUCCESS);
-        stringBuilder.append("Content-Type: text/plain\r\n");
-        stringBuilder.append(String.format("Content-Length: %s\r\n\r\n%s", value.length(), value));
+        stringBuilder.append("Content-Type: text/plain" + NEW_LINE);
+        stringBuilder.append(String.format("Content-Length: %s\r\n%s", value.length(), value));
         return stringBuilder.toString();
     }
 }
