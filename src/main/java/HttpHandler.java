cc559edd87553adfc5360f23c81d3790f8e817e0
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-21 15:47:05 +0300

    test

--- src/main/java/HttpHandler.java
+++ src/main/java/HttpHandler.java
@@ -77,7 +77,7 @@ public class HttpHandler implements Runnable {
             && httpRequest.getHeaders().get("Accept-Encoding").contains("gzip")) {
             stringBuilder.append("Content-Encoding: gzip" + NEW_LINE);
             body = new String(compressResponse(body.getBytes()));
-            stringBuilder.append(String.format("Content-Length: %s%s%s", body.length(), NEW_LINE));
+            stringBuilder.append(String.format("Content-Length: %s%s", body.length(), NEW_LINE));
             stringBuilder.append(body);
             sendResponse(stringBuilder.toString().getBytes());
             return null;
